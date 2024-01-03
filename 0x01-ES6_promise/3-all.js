dbd5b7fe501b405342c24602f6e149f6aa460848
valariembithe <valmbithe691@gmail.com>
2024-01-03 04:27:49 -0800

    Task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise
-    .all([uploadPhoto()], [createUser()])
+    .all([uploadPhoto(), createUser()])
     .then((res) => {
       console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
