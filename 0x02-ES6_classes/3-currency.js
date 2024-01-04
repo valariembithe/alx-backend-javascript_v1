834f4e801cffaf37b549409d16c63c130cfdb38e
valariembithe <valmbithe691@gmail.com>
2024-01-04 14:29:08 +0300

    Task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+  set code(value) {
+    return this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    return this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this.code})`;
+  }
+
+}
