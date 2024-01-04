c06a1dbe6ac4400efbd3762bdfd76cf66bbd0dcd
valariembithe <valmbithe691@gmail.com>
2024-01-04 15:25:55 +0300

    Task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
   constructor(sqft) {
     this.sqft = sqft;
-    if (this.constructor !== 'Building') {
+    if (this.constructor !== Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
         throw new Error(
           'Class extending Building must override evacuationWarningMessage',
@@ -9,6 +9,7 @@ export default class Building {
       } 
     }
   }
+  
   get sqft() {
     return this._sqft;
   }
