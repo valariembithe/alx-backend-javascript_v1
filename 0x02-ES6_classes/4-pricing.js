d8c6019c32a27a78513dd2234e14a3252f10b77e
valariembithe <valmbithe691@gmail.com>
2024-01-04 14:51:48 +0300

    Task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,7 +1,7 @@
 import Currency from "./3-currency";
 
 export default class Pricing {
-	constructor(amount, currency) {
+  constructor(amount, currency) {
 		this.amount = amount;
 		this.currency = currency;
 	}
