b275a33f3dad08616da33d8b9c9b4b9d42b02757
valariembithe <valmbithe691@gmail.com>
2024-01-05 13:12:17 +0300

    Fixed eslint

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -4,28 +4,35 @@ export default class HolbertonCourse {
     this.length = length;
     this.students = students;
   }
-  //Getter
+
   get name() {
     return this._name;
   }
+
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = value;
+    return this._name;
   }
+
   get length() {
     return this._length;
   }
+
   set length(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    return this._length =  value;
+    this._length =  value;
+    return this._length;
   }
+
   get students() {
     return this._students;
   }
+
   set students(value) {
     if (!(value instanceof Array)) {
       throw new TypeError('Students must be an array of strings');
@@ -34,5 +41,6 @@ export default class HolbertonCourse {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = value;
+    return this._students;
   }
 }
