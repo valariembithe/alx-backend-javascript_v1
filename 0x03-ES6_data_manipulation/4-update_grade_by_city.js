d421bf26481bada59f4cdfea77a0d66ced6b1f6f
valariembithe <valmbithe691@gmail.com>
2024-01-08 14:56:24 +0300

    Tasks

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,7 +3,7 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
 
   if(students instanceof Array) {
     return students
-      .filter((student) => student.location === city);
+      .filter((student) => student.location === city)
       .map((student) => ({
         id: student.id,
         firstName: student.firstName,
