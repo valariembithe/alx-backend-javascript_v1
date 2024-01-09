108c732db9cfbb40d2582db818d1478be8658ed9
valariembithe <valmbithe691@gmail.com>
2024-01-09 13:18:12 +0300

    Task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,9 @@ export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  if (map.values  === 1) {
-    return map.set(key, 100);
-  }
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
 }
