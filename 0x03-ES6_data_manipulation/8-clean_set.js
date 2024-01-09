2693063bdb0f99c5c6de25d8ef5ea760b9488344
valariembithe <valmbithe691@gmail.com>
2024-01-09 12:47:18 +0300

    Task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+export default function cleanSet(set, startString) {
+  const parts = [];
+  if (!(set instanceof Set) || !(startString) || !(set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const subString = value.substring(startString.length);
+
+      if (subString && subString !== value) {
+        parts.push(subString);
+      }
+    }    
+  }
+  return parts.join('-');
+}
