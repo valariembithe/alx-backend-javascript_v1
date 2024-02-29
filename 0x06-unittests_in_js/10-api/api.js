63b4526da383a41365609975b1930599473fa3ac
valariembithe <valmbithe691@gmail.com>
2024-02-29 02:23:38 -0800

    Task 10

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const PORT = 7865;
 
+app.use(express.json());
+
 app.get('/', (_, res) => {
   res.send('Welcome to the payment system');
 });
@@ -13,17 +15,17 @@ app.get('/cart/:id(\\d+)', (req, res) => {
   res.send(`Payment methods for cart ${id}`);
 });
 
-app.get('available_payments', (_req, res){
+app.get('/available_payments', (_req, res) => {
   res.json({ payment_methods: { credit_cards: true, paypal: false } });
 });
 
-app.post('/login', (req, res) {
+app.post('/login', (req, res) => {
   let username = '';
   if (req.body) {
     username = req.body.userName;
   }
 
-  res.send(`Welcome ${username}`)
+  res.send(`Welcome ${username}`);
 });
 
 app.listen(PORT, () => {
