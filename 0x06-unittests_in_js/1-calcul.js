e5cb332651518c8e82178b29f882f1e19322bf2c
valariembithe <valmbithe691@gmail.com>
2024-02-29 01:33:16 -0800

    Task files

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -2,10 +2,10 @@ const calculateNumber = (type, a, b) => {
   if(type === 'SUM') {
     return Math.round(a) + Math.round(b);
   }
-  if(type === 'SUB') {
+  if(type === 'SUBTRACT') {
     return Math.round(a) - Math.round(b);
   }
-  if(type === 'DIV') {
+  if(type === 'DIVIDE') {
       return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
     }
   return 0;
